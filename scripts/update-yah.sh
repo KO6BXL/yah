fbe8af16a35ed0c90d0e0490c3cc1374fcf0c94d
ko6bxl <coggeshalladam@gmail.com>
2026-05-22 20:28:28 -0700

    Final fix lol

--- scripts/update-yah.sh
+++ scripts/update-yah.sh
@@ -1,8 +1,9 @@
-bun build --compile --outfile tmp-yah ../src/index.ts
+WORK_DIR="$HOME/.local/share/yah/local-yah"
+bun build --compile --outfile "$WORK_DIR/tmp-yah" "$WORK_DIR/src/index.ts"
 
 kill -s SIGUSR1 $(pidof yah)
 
-if tmp-yah; then 
+if $WORK_DIR/tmp-yah; then 
     echo "Success in running tmp-yah"
 else 
     yah
