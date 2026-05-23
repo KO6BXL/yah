1b932aceb21b554f77cc0087899eabf5e70dc533
ko6bxl <coggeshalladam@gmail.com>
2026-05-22 19:59:09 -0700

    Finalize logic

--- scripts/update-yah.sh
+++ scripts/update-yah.sh
@@ -1,5 +1,7 @@
 bun build --compile --outfile tmp-yah ../src/index.ts
 
+kill -s SIGUSR1 $(pidof yah)
+
 if tmp-yah; then 
     echo "Success in running tmp-yah"
 else 
--- src/index.ts
+++ src/index.ts
@@ -10,13 +10,18 @@ async function cleanUp() {
     process.exit(0)
 }
 
+async function kill() {
+    process.exit(0)
+}
+
 process.on("SIGINT", cleanUp)
 process.on("SIGTERM", cleanUp)
+process.on("SIGUSR1", kill)
 
 SecretStore.init()
 const agent = await MainAgent.create()
 console.log("hi")
 if(await FileStore.Exists("session/session.jsonl")) {
-    await agent.prompt("Program crash: session reinstated")
+    await agent.prompt("Program stopped: session reinstated")
 }
 await agent.start()
